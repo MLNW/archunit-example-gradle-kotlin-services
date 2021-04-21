8f24ff09eaaff270e0929824d09bc4c25a19ef74
Lucas Resch <lucas.resch@cgi.com>
2021-04-21 09:47:27 +0200

    Implement ArchUnit test

---
+++ services/tests/src/test/kotlin/ArchitectureTests.kt
@@ -0,0 +1,22 @@
+import com.tngtech.archunit.core.domain.JavaClasses
+import com.tngtech.archunit.core.importer.ImportOption
+import com.tngtech.archunit.junit.AnalyzeClasses
+import com.tngtech.archunit.junit.ArchTest
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@AnalyzeClasses(
+    packages = ["example"],
+    importOptions = [ImportOption.DoNotIncludeTests::class],
+)
+class ArchitectureTests {
+
+    @ArchTest
+    fun `Check service application starters`(classes: JavaClasses) {
+        print(classes)
+        ArchRuleDefinition.classes()
+            .that().areAnnotatedWith(SpringBootApplication::class.java)
+            .should().haveSimpleNameEndingWith("Starter")
+            .check(classes)
+    }
+}
